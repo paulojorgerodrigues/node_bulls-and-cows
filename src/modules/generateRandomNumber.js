370c234bd6f70cc015661dad41f2bec215dca383
Paulo Rodrigues <paulo.jorge.rodrigues@gmail.com>
2025-04-01 00:23:56 +0100

    solution

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -1,5 +1,9 @@
 'use strict';
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 /**
  * Generate a random 4-digit number that does not start with 0
  * and does not contain any duplicate digits.
@@ -8,6 +12,32 @@
  */
 function generateRandomNumber() {
   /* Write your code here */
+  let str = '';
+  let i = 0;
+
+  while (i < 4) {
+    let n;
+
+    if (i === 0) {
+      n = getRandomInt(8) + 1;
+
+      const sn = `${n}`;
+
+      str += sn;
+      i++;
+    } else {
+      n = getRandomInt(9) + 1;
+
+      const sn = `${n}`;
+
+      if (str.indexOf(sn) < 0) {
+        str += sn;
+        i++;
+      }
+    }
+  }
+
+  return +str;
 }
 
 module.exports = {
